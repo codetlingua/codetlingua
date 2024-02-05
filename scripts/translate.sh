9cd33855b084d3561dcb733cf62329a363e9ba8c
Michele <michele.merler@gmail.com>
2024-02-05 18:11:06 -0500

    added prompt type to translate.sh

--- scripts/translate.sh
+++ scripts/translate.sh
@@ -5,11 +5,12 @@ export PYTHONIOENCODING=utf-8;
 
 function prompt() {
     echo;
-    echo "Syntax: bash scripts/translate.sh MODEL DATASET SRC_LANG TRG_LANG TEMPERATURE N_SAMPLES B_SIZE MAX_LENGTH NUM_GPUS GPU_ID";
+    echo "Syntax: bash scripts/translate.sh MODEL DATASET SRC_LANG TRG_LANG PROMPT_TYPE TEMPERATURE N_SAMPLES B_SIZE MAX_LENGTH NUM_GPUS GPU_ID";
     echo "MODEL: name of the model to use";
     echo "DATASET: name of the dataset to use";
     echo "SRC_LANG: source language";
     echo "TRG_LANG: target language";
+    echo "PROMPT_TYPE: prompt format";
     echo "TEMPERATURE: temperature for sampling";
     echo "N_SAMPLES: number of samples to generate";
     echo "B_SIZE: batch size";
@@ -26,7 +27,7 @@ while getopts ":h" option; do
     esac
 done
 
-if [[ $# < 10 ]]; then
+if [[ $# < 11 ]]; then
   prompt;
 fi
 
@@ -34,12 +35,13 @@ MODEL=$1
 DATASET=$2
 SRC_LANG=$3
 TRG_LANG=$4
-TEMPERATURE=$5
-N_SAMPLES=$6
-B_SIZE=$7
-MAX_LENGTH=$8
-NUM_GPUS=$9
-GPU_ID=${10}
+PROMPT_TYPE=$5
+TEMPERATURE=$6
+N_SAMPLES=$7
+B_SIZE=$8
+MAX_LENGTH=$9
+NUM_GPUS=$10
+GPU_ID=${11}
 
 export CUDA_VISIBLE_DEVICES=$GPU_ID;
-python3 translate/translate.py --model=$MODEL --dataset=$DATASET --source_lang=$SRC_LANG --target_lang=$TRG_LANG --temperature=$TEMPERATURE --n_samples=$N_SAMPLES --batch_size=$B_SIZE --max_length=$MAX_LENGTH --ngpus=$NUM_GPUS --resume
+python3 translate/translate.py --model=$MODEL --dataset=$DATASET --source_lang=$SRC_LANG --target_lang=$TRG_LANG --prompt_type=$PROMPT_TYPE --temperature=$TEMPERATURE --n_samples=$N_SAMPLES --batch_size=$B_SIZE --max_length=$MAX_LENGTH --ngpus=$NUM_GPUS --resume
